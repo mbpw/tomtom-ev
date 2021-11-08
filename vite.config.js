2813d20ec44d218a26e3898776c4d80854dccfb3
Kuba <kubalobo@gmail.com>
2021-11-08 18:20:49 +0100

    Preparations for PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [
+      svelte(),
+      // TODO: Configure PWA with these documentation https://vite-plugin-pwa.netlify.app/guide/generate.html
+      VitePWA({})
+  ]
 })
