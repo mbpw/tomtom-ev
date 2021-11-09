0858ab13f52c2083c95bac37d1a35be8572f66f0
Kuba <kubalobo@gmail.com>
2021-11-09 15:02:21 +0100

    App layout core

--- src/App.svelte
+++ src/App.svelte
@@ -1,58 +1,53 @@
 <script>
-  import logo from './assets/giphy.gif'
-  import Counter from './Components/Counter.svelte'
-  import {RouteGenerator} from './computing_engine/route-generator'
-  let route_info = 'not computed'
-  let result = 'nope';
-  let distance = 0;
-  let rg = new RouteGenerator();
+    import {RouteGenerator} from './computing_engine/route-generator'
+    import Layout from "./Layout.svelte";
 
-  async function compute_route() {
-    let route = await rg.getNextRoute()
-    console.log(route)
-    route_info=route.routes[0].summary.travelTimeInSeconds/3600
+    let route_info = 'not computed'
+    let result = 'nope';
+    let distance = 0;
+    let rg = new RouteGenerator();
 
-  }
+    let kamil = true;
 
-  async function get_next_poi() {
-    let routeOffer = await rg.prepareRouteOffer()
-    console.log(routeOffer)
-    result = routeOffer.POIs[0].poi.name
-    distance = routeOffer.POIs[0].route[0].summary.lengthInMeters
+    async function compute_route() {
+        let route = await rg.getNextRoute()
+        console.log(route)
+        route_info = route.routes[0].summary.travelTimeInSeconds / 3600
+    }
 
-  }
-
+    async function get_next_poi() {
+        let routeOffer = await rg.prepareRouteOffer()
+        console.log(routeOffer)
+        result = routeOffer.POIs[0].poi.name
+        distance = routeOffer.POIs[0].route[0].summary.lengthInMeters
+    }
 </script>
+{#if kamil}
+    <main>
+        <button on:click={() => kamil = !kamil}>
+            Wyłącz Kamila
+        </button>
 
-<main>
-  <img src={logo} alt="Svelte Logo" />
-  <h1>Hello world!!!</h1>
+        <h1>Hello world!!!</h1>
 
-  <Counter />
-  <p>
-    Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte
-    apps.
-  </p>
-  <p>
-    <button on:click={compute_route}>
-      Compute next optimal route!, time (h) = {route_info}
-    </button>
+        <p>
+            <button on:click={compute_route}>
+                Compute next optimal route!, time (h) = {route_info}
+            </button>
 
-    <button on:click={get_next_poi}>
-      Change POI: {result} + distance = {distance}
-    </button>
+            <button on:click={get_next_poi}>
+                Change POI: {result} + distance = {distance}
+            </button>
+        </p>
+    </main>
+{:else}
+    <Layout/>
+{/if}
 
-  </p>
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for
-    the officially supported framework, also powered by Vite!
-  </p>
-</main>
-
-<style>
+<style lang="scss">
   :root {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
-      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
 
   main {
@@ -61,34 +56,13 @@
     margin: 0 auto;
   }
 
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
   h1 {
-    color: #66ff00;
+    color: $primary;
     text-transform: uppercase;
     font-size: 4rem;
     font-weight: 100;
     line-height: 1.1;
     margin: 2rem auto;
     max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      max-width: none;
-    }
-
-    p {
-      max-width: none;
-    }
   }
 </style>
