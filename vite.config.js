cede8442c3a3635525cc7533622bd2d7c70e91a4
Kuba <kubalobo@gmail.com>
2021-11-09 13:28:29 +0100

    static files location config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { VitePWA } from 'vite-plugin-pwa'
+import {defineConfig} from 'vite'
+import {svelte} from '@sveltejs/vite-plugin-svelte'
+import {VitePWA} from 'vite-plugin-pwa'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-      svelte(),
-      // TODO: Configure PWA with these documentation https://vite-plugin-pwa.netlify.app/guide/generate.html
-      VitePWA({})
-  ]
+    plugins: [
+        svelte(),
+        // TODO: Configure PWA with these documentation https://vite-plugin-pwa.netlify.app/guide/generate.html
+        VitePWA({})
+    ],
+    publicDir: 'public',
+    base: '',
+    build: {
+        outDir: 'dist',
+        // minify: production,
+    }
 })
