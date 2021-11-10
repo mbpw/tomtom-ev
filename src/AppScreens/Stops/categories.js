85b9040a3cd53af1392a0bf5f874bc921eb4f1e8
Mateusz <mateusz.bialek@o2.pl>
2021-11-10 15:37:32 +0100

    add some categories

--- src/AppScreens/Stops/categories.js
+++ src/AppScreens/Stops/categories.js
@@ -3,54 +3,60 @@ export const categories = [
         name: 'Food',
         icon: './icons/food.svg',
         options: [
-            'Burger',
-            'Kebab',
-            'Pizza',
+            {name: 'Burger', category_id: '7315069'},
+            {name: 'Sandwich', category_id: '7315042'},
+            {name: 'Pizza', category_id: '7315036'},
+            {name: 'Any', category_id: '7315'}
         ]
     },
     {
         name: 'Sleep',
         icon: './icons/sleep.svg',
         options: [
-            'Hotel',
-            'Hostel/Motel',
-            'Camping',
+            {name: 'Hotel', category_id: '7314003'},
+            {name: 'Hostel/Motel/B&B', category_id: '7314004,7314006,7314002'},
+            {name: 'Camping', category_id: '7360'},
+            {name: 'Any', category_id: '7314,7360'}
         ]
     },
     {
         name: 'Activity',
         icon: './icons/activity.svg',
         options: [
-            'Hotel',
-            'Hostel/Motel',
-            'Camping',
+            {name: 'Walk/Run', category_id: '9362002,9362015,9362004,9362005,9362006,9362030,9362008,9362033,9362011,9362013,9362014,9362036,7302004,7302005,8099'},
+            {name: 'Gym/Fitness/Sports', category_id: '7320002,7305'},
+            {name: 'Fun (nightlife, movies, theater, ...)', category_id: '9362017,9379,7342,7318'},
+            {name: 'Any', category_id: ''}
         ]
     },
     {
         name: 'Sightseeing',
         icon: './icons/sightseeing.svg',
         options: [
-            'Hotel',
-            'Hostel/Motel',
-            'Camping',
+            {name: 'Museum/Gallery', category_id: '7317'},
+            {name: 'Natural attraction', category_id: '7376004,9362036,9362004,9362030,8099'},
+            {name: 'Building/Architecture', category_id: '7376012,7376010,7376002,7376007,7376011,7376003,7376005,7376006,7376013,7376009,7376008,7376014,'},
+            {name: 'Any', category_id: '7317,7376,9362036,'}
         ]
     },
     {
         name: 'Work',
         icon: './icons/work.svg',
         options: [
-            'Hotel',
-            'Hostel/Motel',
-            'Camping',
+            {name: 'Cafe', category_id: ''},
+            {name: '', category_id: ''},
+            {name: '', category_id: ''},
+            {name: 'Any', category_id: ''}
         ]
     },
     {
         name: 'Shopping',
         icon: './icons/shopping.svg',
         options: [
-            'Hotel',
-            'Hostel/Motel',
-            'Camping',
+            {name: '', category_id: ''},
+            {name: '', category_id: ''},
+            {name: '', category_id: ''},
+            {name: 'Any', category_id: ''}
         ]
     },
 ]
