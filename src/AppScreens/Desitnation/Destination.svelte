e8df4394ab133449c13f398b507e09854d7205d2
Kuba <kubalobo@gmail.com>
2021-11-11 10:54:25 +0100

    Input reset button

--- src/AppScreens/Desitnation/Destination.svelte
+++ src/AppScreens/Desitnation/Destination.svelte
@@ -4,7 +4,8 @@
     import TimePickerButton from "./TimePickerButton.svelte";
     import DotsSeparator from "./DotsSeparator.svelte";
     import Button from "../../Components/Button.svelte";
-    import {carProfileStore, endPointStore, startPointStore} from "../../stores/userInput";
+    import {carProfileStore, endPointStore, startDateStore, startPointStore} from "../../stores/userInput";
+
     import {chargingStops} from "../../stores/routesInfo";
     import {openedScreen} from "../../stores/appState";
     import {RG} from "../../computing_engine/route-generator";
@@ -13,13 +14,15 @@
     import {getContext} from "svelte";
     import MapPickerModal from "./MapPickerModal.svelte";
     import CarSelectModal from "./CarSelectModal.svelte";
+    import {carProfiles} from "./carProfiles";
 
     const {open} = getContext('simple-modal');
 
-    async function computeChargingStopsNumber(){
-        $chargingStops = await RG.computeOptimalRouteSize($startPointStore.latlng,$endPointStore.latlng)
+    async function computeChargingStopsNumber() {
+        $chargingStops = await RG.computeOptimalRouteSize($startPointStore.latlng, $endPointStore.latlng)
     }
 
+    let waiting = false;
 </script>
 
 <Header>
@@ -64,14 +67,43 @@
 
 {#if !$chargingStops}
     <Button on:click={() => {
-    console.log('Pocisk')
-    if($chargingStops)
+        waiting = true;
+        computeChargingStopsNumber();
+    }}>
+        {#if waiting}
+            Waiting...
+        {:else}
+            Go!
+        {/if}
+    </Button>
+{:else}
+    <Button on:click={() => {
         $chargingStops = 0;
-    else
-        $chargingStops = 4;
-    computeChargingStopsNumber()
+        waiting = false;
 
+        $startDateStore = new Date();
+        $startPointStore = {
+            address: 'Choose a start point',
+            latlng: {
+                lat: 52.32563573919947,
+                lng: 10.523825676170611
+            }
+        };
+        $endPointStore = {
+            address: 'Choose an end point',
+            latlng: {
+                lat: 52.509548827862005,
+                lng: 13.62762775333342
+            }
+        };
+        $carProfileStore = ({
+            name: 'Select car profile',
+            // body: undefined,
+            // params: undefined
+            body: carProfiles[1].body,
+            params: carProfiles[1].params
+        });
 }}>
-        Go!
+        Reset
     </Button>
 {/if}
