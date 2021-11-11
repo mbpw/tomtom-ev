9fa40e699868553b75ce1a328fcd9d075470ca3e
Kuba <kubalobo@gmail.com>
2021-11-11 10:04:37 +0100

    Default points

--- src/stores/userInput.js
+++ src/stores/userInput.js
@@ -3,11 +3,22 @@ import { writable } from 'svelte/store';
 export const startDateStore = writable(new Date());
 export const startPointStore = writable({
     address: 'Choose a start point',
-    latlng: null
+    latlng: {
+        lat: 52,
+        lng: 21
+    }
 });
 export const endPointStore = writable({
     address: 'Choose an end point',
-    latlng: null
+    latlng: {
+        lat: 53,
+        lng: 22
+    }
+});
+
+export const carProfileStore = writable({
+    name: 'Select car profile',
+    chargingParameters: undefined
 });
 
 export const stopsPreferences = writable([]);
